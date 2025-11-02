d09c6bc97bbd4eabecc6e505c6057a1fe3901acc
Amine <amine.khiri@etu.umontpellier.fr>
2025-11-02 09:02:02 +0100

    front et back

---
+++ backend/dist/db/database.js
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import pkg from 'pg';
+const { Pool } = pkg;
+// Récupération de la variable d'environnement Docker
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL ||
+        'postgres://secureapp:secureapp@localhost:5432/secureapp',
+});
+export default pool;
+//# sourceMappingURL=database.js.map
---
+++ backend/src/types/token-payload.ts
@@ -0,0 +1,9 @@
+
+export interface TokenPayload {
+
+    id: number
+    role: string
+    iat?: number // issued at
+    exp?: number // expiration
+    
+}
